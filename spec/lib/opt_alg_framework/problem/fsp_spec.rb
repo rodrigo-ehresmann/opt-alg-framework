96484aff767f65a8d8a7e2c69f9059f6c57dec4e
rodrigo-ehresmann <igoehresmann@gmail.com>
2016-09-10 00:25:21 -0300

    Add tests to FSP problem

---
+++ spec/lib/opt_alg_framework/problem/fsp_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe Problem::FSP do
+  describe "#fitness" do
+    let(:problem) { Problem::FSP.new }
+
+    context "when normal reading instances" do
+      let(:instances) {
+                        { "3x2" => { [0, 1] => 14 } }
+                        { "3x2" => { [1, 0] => 12 } }
+                        { "2x5" => { [0, 1, 2, 3, 4] => 20 } }
+                        { "2x5" => { [0, 4, 2, 3, 1] => 18 } }
+                      }
+
+      it "should return the right makespan" do
+        instances.each do |instance, solution|
+          problem.load_instance("#{Dir.pwd}/spec/instances/#{instance}.txt")
+          solution.each do |tasks_sequence, result|
+            expect(problem.fitness(tasks_sequence)).to eq(result)
+          end
+        end
+      end
+    end
+
+    context "when inform to transpose the instance after read" do
+      let(:transposed_instances) {
+                                    { "3x2" => { [0, 1, 2] => 14 } }
+                                    { "3x2" => { [2, 1, 0] => 12 } }
+                                    { "2x5" => { [0, 1] => 20 } }
+                                    { "2x5" => { [1, 0] => 19 } }
+                                 }
+
+      it "should return the right makespan" do
+        transposed_instances.each do |instance, solution|
+          problem.load_instance("#{Dir.pwd}/spec/instances/#{instance}.txt") { true }
+          solution.each do |tasks_sequence, result|
+            expect(problem.fitness(tasks_sequence)).to eq(result)
+          end
+        end
+      end
+    end
+  end
+end
