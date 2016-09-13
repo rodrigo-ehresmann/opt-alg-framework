27731a7bd3f28ef224c6d1a969172756f51e40bf
rodrigo-ehresmann <igoehresmann@gmail.com>
2016-09-13 13:25:00 -0300

    Reorganize requires

--- lib/opt_alg_framework.rb
+++ lib/opt_alg_framework.rb
@@ -1,6 +1,16 @@
-valid_files = Dir["#{File.dirname(__FILE__)}/**/*.rb"].reject {|f| File.directory?(f) }
-valid_files.each { |f| require f }
+require 'opt_alg_framework/algorithm/algorithm_interface'
+require 'opt_alg_framework/problem/problem_interface'
+require 'opt_alg_framework/operator/tweak/tweak_interface'
+require 'opt_alg_framework/operator/selector/selector_interface'
+require 'opt_alg_framework/operator/crossover/permutation/permutation_crossover_interface'
+
+Dir[File.dirname(__FILE__) + "/opt_alg_framework/problem/**/*.rb"].each { |f| require f }
+Dir[File.dirname(__FILE__) + "/opt_alg_framework/operator/**/*.rb"].each { |f| require f }
+Dir[File.dirname(__FILE__) + "/opt_alg_framework/algorithm/**/*.rb"].each { |f| require f }
+
+#valid_files = Dir["#{File.dirname(__FILE__)}/**/*.rb"].reject {|f| File.directory?(f) }
+#valid_files.each { |f| require f }
 
 module OptAlgFramework
-  # Write your code here
+
 end
